27cf7d49dcf565ddf487478da899d00a22aadfae
ChainsawFrogge <g.turanli@student.utwente.nl>
2026-04-20 20:39:23 +0200

    ocr update

--- ocr.py
+++ ocr.py
@@ -1,6 +1,7 @@
 import pytesseract
 import cv2
 
+pytesseract.pytesseract.tesseract_cmd = "/opt/homebrew/bin/tesseract"
 def extract_text(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     return pytesseract.image_to_string(gray)
