b24cf35bb1abeab8e8cef3b813c75ec4561d0a49
ChainsawFrogge <g.turanli@student.utwente.nl>
2026-04-20 20:44:02 +0200

    updates

--- main.py
+++ main.py
@@ -3,12 +3,20 @@ from ocr import extract_text
 from translate import translate
 import time
 
+
+def is_valid(text):
+    text = text.strip()
+    return len(text) > 5 and not text.isdigit()
+
 seen = set()
 
 while True:
     img = capture_screen()
     text = extract_text(img)
-
+    
+    if text.strip() and is_valid(text):
+        print("Original:", text)
+    
     if text.strip() and text not in seen:
         seen.add(text)
         translated = translate(text)
--- ocr.py
+++ ocr.py
@@ -3,11 +3,23 @@ import cv2
 
 pytesseract.pytesseract.tesseract_cmd = "/opt/homebrew/bin/tesseract"
 
-def extract_text(img):
+def extract_text_boxes(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    gray = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
-    config = r"--psm 6"
-    text = pytesseract.image_to_string(gray, config=config)
+    data = pytesseract.image_to_data(gray, output_type=pytesseract.Output.DICT)
 
-    return text
+    boxes = []
+
+    for i in range(len(data["text"])):
+        text = data["text"][i].strip()
+        conf = int(data["conf"][i])
+
+        if conf > 60 and len(text) > 1:
+            x, y, w, h = data["left"][i], data["top"][i], data["width"][i], data["height"][i]
+
+            boxes.append({
+                "text": text,
+                "box": (x, y, w, h)
+            })
+
+    return boxes
