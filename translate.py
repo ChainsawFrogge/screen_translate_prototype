54e9b9e93d4ef00d22eb6defc01a54dfddb772fc
ChainsawFrogge <g.turanli@student.utwente.nl>
2026-04-20 20:45:10 +0200

    translate change

--- translate.py
+++ translate.py
@@ -1,4 +1,15 @@
 from deep_translator import GoogleTranslator
 
+cache = {}
+
 def translate(text):
-    return GoogleTranslator(source='auto', target='en').translate(text)
+    if text in cache:
+        return cache[text]
+
+    translated = GoogleTranslator(
+        source='auto',
+        target='en'
+    ).translate(text)
+
+    cache[text] = translated
+    return translated
