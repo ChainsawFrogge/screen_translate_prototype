9be785ffc3cbb7204c2317c9df01283eb72b3241
ChainsawFrogge <g.turanli@student.utwente.nl>
2026-04-20 20:40:52 +0200

    ocr update v2

--- ocr.py
+++ ocr.py
@@ -2,6 +2,12 @@ import pytesseract
 import cv2
 
 pytesseract.pytesseract.tesseract_cmd = "/opt/homebrew/bin/tesseract"
+
 def extract_text(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    return pytesseract.image_to_string(gray)
+    gray = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+    config = r"--psm 6"
+    text = pytesseract.image_to_string(gray, config=config)
+
+    return text
